33f78670e7e729cf0066152f34c6f84e498b69f0
Luke <lsh-0@users.noreply.github.com>
2019-02-06 13:47:40 +1030

    added derived field 'component-doi' adds dependency on tqdm

--- load.py
+++ load.py
@@ -1,7 +1,9 @@
+from tqdm import tqdm
 from collections import OrderedDict
 import sqlite3
 import json
 import glob
+import re
 
 def nsdict(d, k):
     if k not in d:
@@ -16,7 +18,18 @@ def nsdict(d, k):
 def subdict(d, ks):
     return {k:v for k, v in d.items() if k in ks}
 
-keys = ['obj_id', 'occurred_at', 'subj_id', 'id', 'evidence_record', 'relation_type_id', 'subj__pid', 'subj__url', 'subj__type', 'subj__title', 'obj__pid', 'obj__url']
+keys = [
+    'obj_id', 'occurred_at', 'subj_id', 'id', 'evidence_record', 'relation_type_id', 'subj__pid', 'subj__url', 'subj__type', 'subj__title', 'obj__pid', 'obj__url',
+
+    # derived fields
+    'component_doi'
+]
+
+def extract_doi_component(doi):
+    regex = r"elife\..+\.(\d+)$"
+    result = re.findall(regex, doi)
+    if result:
+        return result[0]
 
 def mkrow(row):
     keepers = ['id', 'subj_id', 'relation_type_id', 'obj_id', 'occurred_at', 'evidence_record', 'subj', 'obj']
@@ -24,6 +37,7 @@ def mkrow(row):
     nsdict(new_row, 'subj')
     nsdict(new_row, 'obj')
     new_row = subdict(new_row, keys)
+    new_row['component_doi'] = extract_doi_component(new_row['obj_id'])
     return new_row
 
 def create_rows(fname):
@@ -53,5 +67,5 @@ cursor = conn.cursor()
 cursor.execute(sql_create_table)
 conn.commit()
 
-for fname in glob.glob('event-data-*'):
+for fname in tqdm(glob.glob('event-data-*'), unit='files'):
     insert_many_rows(conn, cursor, create_rows(fname))
